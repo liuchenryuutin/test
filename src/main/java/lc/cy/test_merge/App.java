a316454e19d5cd57ea2196fd972b45a2f6f5ed88
liuchen <1015383369@qq.com>
2019-03-06 21:17:34 +0800

    第一次提交

---
+++ src/main/java/lc/cy/test_merge/App.java
@@ -0,0 +1,13 @@
+package lc.cy.test_merge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/lc/cy/test_merge/test.java
@@ -0,0 +1,9 @@
+package lc.cy.test_merge;
+
+public class test {
+
+	public void say() {
+		System.out.println("Are you OK!");
+	}
+
+}
